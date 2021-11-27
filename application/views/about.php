9c013782ee43c34a07b54563c2febff1b2d1607c
ahauwlie <38755351+ahauwlie@users.noreply.github.com>
2021-11-27 19:15:38 +0700

    upup

--- application/views/about.php
+++ application/views/about.php
@@ -1,32 +1,32 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: Hauw
-     * Date: 9/26/2018
-     * Time: 9:11 AM
-     */
-    defined('BASEPATH') OR exit('No direct script access allowed');
-    ?>
+	/**
+	 * Created by PhpStorm.
+	 * User: Hauw
+	 * Date: 9/26/2018
+	 * Time: 9:11 AM
+	 */
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Arquito - 3D Architecture &amp; Interior HTML Template">
-        <meta name="author" content="Paul, Logan Cee, Mikhail Ojereliev">
-        <title>Aura Putra Wijaya</title>
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Teko:300,400" rel="stylesheet">
-        <?php echo $css; ?>
-        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
-        <link rel="manifest" href="../favicons/site.webmanifest">
-        <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
-        <link rel="shortcut icon" href="favicons/favicon.ico">
-        <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="msapplication-config" content="favicons/browserconfig.xml">
-        <meta name="theme-color" content="#ffffff">
-    </head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Arquito - 3D Architecture &amp; Interior HTML Template">
+		<meta name="author" content="Paul, Logan Cee, Mikhail Ojereliev">
+		<title>Aura Putra Wijaya</title>
+		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Teko:300,400" rel="stylesheet">
+		<?php echo $css; ?>
+		<link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+		<link rel="manifest" href="../favicons/site.webmanifest">
+		<link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="shortcut icon" href="favicons/favicon.ico">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="msapplication-config" content="favicons/browserconfig.xml">
+		<meta name="theme-color" content="#ffffff">
+	</head>
 	<body class="page">
 		<div class="page__inner animsition">
 			<?php echo $header; ?>
@@ -38,13 +38,22 @@
 					<div class="faded-block main-slide__container container">
 						<div class="main-slide__slide-inner" style="color: black;">
 							<div class="main-slide__subtitle-wrapper">
-								<div class="main-slide__subtitle">Isi apa hayooo</div>
+								<div class="main-slide__subtitle">Tentang Kami</div>
 							</div>
 							<div class="main-slide__title-wrapper">
-								<div class="main-slide__title">Isi apa hayooo<br> Isi apa hayooo</div>
+								<div class="main-slide__title">PT. Aura Putra Wijaya</div>
 							</div>
 							<div class="main-slide__text-wrapper">
-								<div class="main-slide__text">Isi apa hayooo</div>
+								<div class="main-slide__text">Didirikan pada tahun 2005 di Bandung, Jawa Barat.
+									Dengan nama sebelumnya CV. Aura Putra Wijaya.
+									<br>Perusahaan kami mengkhususkan diri bergerak dibidang industri dan
+									pengadaan seragam, baik tutup kepala, tutup badan, tutup kaki, accesories
+									maupun produk lainnya sesuai dengan pesanan.
+									<br>Perusahaan kami selalu berinovasi mengembangkan produk-produk
+									yang inovatif seiring dengan perkembangan zaman untuk menghasilkan produk
+									dengan kualitas terbaik.
+									karena bagi kami, kualitas adalah prioritas
+								</div>
 							</div>
 						</div>
 					</div>
@@ -54,69 +63,78 @@
 						<div class="row">
 							<div class="col-12 col-xs-6 col-lg-5 col-xl-4">
 								<div class="counter-block__card">
-									<div class="counter-block__card-title">our<br />history</div>
-									<div data-counter-value="25" class="counter-block__card-value js-counter">1</div>
-									<div class="counter-block__card-description">years<br />experience</div>
+									<div class="counter-block__card-title">Sudah<br/>Sekitar</div>
+									<div data-counter-value="16" class="counter-block__card-value js-counter"></div>
+									<div class="counter-block__card-description">Tahun<br/>Kami Berdiri</div>
 								</div>
 							</div>
-							<div class="col-12 col-xs-6 col-lg-5 col-xl-4 offset-lg-1 offset-xl-2">
-								<div class="counter-block__title heading-smallest">our achievements</div>
-								<div class="counter-block__item">
-									<div data-counter-value="12000" class="counter-block__item-value js-counter">12,000</div>
-									<div class="counter-block__item-text">Ceritanya angka pencapaian</div>
+							<div class="col-12 col-xs-6 col-lg-5 col-xl-5 offset-lg-1 offset-xl-2">
+								<div class="counter-block__title heading-smallest">LINGKUP
+									PEKERJAAN
 								</div>
 								<div class="counter-block__item">
-									<div data-counter-value="4620" class="counter-block__item-value js-counter">4,620</div>
-									<div class="counter-block__item-text">Ceritanya angka pencapaian</div>
+									<div class="counter-block_2_item-value js-counter">Manufaktur</div>
+									<div class="counter-block__item-text">Berbasis ide dan inovasi dengan mengembangkan dari
+										bahan baku untuk mencapai kualitas yang berorientasi
+										pada kesempurnaan produk akhir.
+									</div>
 								</div>
 								<div class="counter-block__item">
-									<div data-counter-value="2750" class="counter-block__item-value js-counter">2,750</div>
-									<div class="counter-block__item-text">Ceritanya angka pencapaian</div>
+									<div class="counter-block_2_item-value js-counter">Perdagangan</div>
+									<div class="counter-block__item-text">Menjaga hubungan baik dengan klien dan memperluas cakupan
+										untuk meningkatkan penjualan.
+									</div>
+								</div>
+								<div class="counter-block__item">
+									<div class="counter-block_2_item-value js-counter">Penelitian & Pengembangan</div>
+									<div class="counter-block__item-text">Menciptakan bentuk bahan atau produk baru untuk memberikan
+										nilai tambah pada produk yang sudah ada maupun di masa depan.
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 				<div class="container">
-					<div class="heading-smallest heading-smallest heading-smallest_has-offset heading-smallest heading-smallest_size_default">Our specialization</div>
+					<div class="heading-smallest heading-smallest heading-smallest_has-offset heading-smallest heading-smallest_size_default">PRODUKSI KAMI</div>
 				</div>
 				<div class="cards">
 					<div class="container-fluid">
 						<div class="row">
 							<div class="cards__item col-12 col-md-6 col-xl-3">
 								<a style="background-image: url(../inti/img/zoom-slide-image-1.jpg)" href="#" class="cards__item-inner">
-									<span class="cards__item-content">
-										<span class="cards__item-index">01</span>
-										<span class="cards__item-title">Isi apa hayooooo</span>
-										<span class="cards__item-text">Isi apa hayooooo</span>
-									</span>
+								<span class="cards__item-content">
+								<span class="cards__item-index">01</span>
+								<span class="cards__item-title">Isi apa hayooooo</span>
+								<span class="cards__item-text">Isi apa hayooooo</span>
+								</span>
 								</a>
 							</div>
 							<div class="cards__item col-12 col-md-6 col-xl-3">
 								<a style="background-image: url(../inti/img/zoom-slide-image-2.jpg)" href="#" class="cards__item-inner">
-									<span class="cards__item-content">
-										<span class="cards__item-index">02</span>
-										<span class="cards__item-title">Isi apa hayooooo</span>
-										<span class="cards__item-text">Isi apa hayooooo</span>
-									</span>
+								<span class="cards__item-content">
+								<span class="cards__item-index">02</span>
+								<span class="cards__item-title">Isi apa hayooooo</span>
+								<span class="cards__item-text">Isi apa hayooooo</span>
+								</span>
 								</a>
 							</div>
 							<div class="cards__item col-12 col-md-6 col-xl-3">
 								<a style="background-image: url(../inti/img/zoom-slide-image-3.jpg)" href="#" class="cards__item-inner">
-									<span class="cards__item-content">
-										<span class="cards__item-index">03</span>
-										<span class="cards__item-title">Isi apa hayooooo</span>
-										<span class="cards__item-text">Isi apa hayooooo</span>
-									</span>
+								<span class="cards__item-content">
+								<span class="cards__item-index">03</span>
+								<span class="cards__item-title">Isi apa hayooooo</span>
+								<span class="cards__item-text">Isi apa hayooooo</span>
+								</span>
 								</a>
 							</div>
 							<div class="cards__item col-12 col-md-6 col-xl-3">
 								<a style="background-image: url(../inti/img/zoom-slide-image-4.jpg)" href="#" class="cards__item-inner">
-									<span class="cards__item-content">
-										<span class="cards__item-index">04</span>
-										<span class="cards__item-title">Isi apa hayooooo</span>
-										<span class="cards__item-text">Isi apa hayooooo</span>
-									</span>
+								<span class="cards__item-content">
+								<span class="cards__item-index">04</span>
+								<span class="cards__item-title">Isi apa hayooooo</span>
+								<span class="cards__item-text">Isi apa hayooooo</span>
+								</span>
 								</a>
 							</div>
 						</div>
@@ -160,8 +178,8 @@
 							</div>
 							<div class="col-12 col-lg-6 offset-lg-1">
 								<a href="https://www.youtube.com/watch?v=YCcVhtolK7g" target="_blank" class="video-block popup-video">
-									<img src="<?php echo base_url('/inti/img/zoom-image-head-bg2.png'); ?>" alt="" />
-									<span class="video-block__play"></span>
+								<img src="<?php echo base_url('/inti/img/zoom-image-head-bg2.png'); ?>" alt="" />
+								<span class="video-block__play"></span>
 								</a>
 							</div>
 						</div>
@@ -169,33 +187,32 @@
 				</div>
 				<div class="awards-block">
 					<div class="container">
-						<div class="heading-smallest heading-smallest heading-smallest_has-offset heading-smallest heading-smallest_size_default">Our winning</div>
+						<div class="heading-smallest heading-smallest heading-smallest_has-offset heading-smallest heading-smallest_size_default">PT. Aura Putra Wijaya</div>
 						<div class="awards-block__body awards-block__body awards-block__body_bordered">
 							<div class="awards-block__item">
-								<div class="awards-block__year">2019</div>
+								<div class="awards-block__year">Mengembangkan <br>5 macam kategori produk yaitu:</div>
 								<div class="awards-block__list">
 									<div class="awards-block__text">
-										Isi apa hayoooo
-										<div class="awards-block__text-footer">Isi apa hayoooo</div>
+										Tutup Kepala
 									</div>
 									<div class="awards-block__text">
-										Isi apa hayoooo
-										<div class="awards-block__text-footer">Isi apa hayoooo</div>
+										Tutup Badan
 									</div>
 									<div class="awards-block__text">
-										Isi apa hayoooo
-										<div class="awards-block__text-footer">Isi apa hayoooo</div>
+										Tutup Kaki
 									</div>
 									<div class="awards-block__text">
-										Isi apa hayoooo
-										<div class="awards-block__text-footer">Isi apa hayoooo</div>
+										Aksesoris
+									</div>
+									<div class="awards-block__text">
+										Produk Lainnya Sesuai Pesanan
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div class="team-block team-block team-block_separated">
+				<!-- <div class="team-block team-block team-block_separated">
 					<div class="container">
 						<div class="team-block__body">
 							<div class="heading-smallest heading-smallest heading-smallest_has-offset heading-smallest heading-smallest_size_default">Meet our team</div>
@@ -229,7 +246,7 @@
 							<div class="team-block__text">Become a member in Aura Team! Send your CV to our email: <a href="#"><span class="__cf_email__">email@email.com</span></a></div>
 						</div>
 					</div>
-				</div>
+					</div> -->
 			</main>
 			<?php echo $footer; ?>
 		</div>
